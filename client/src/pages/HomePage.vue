0ec3da122d27bc1f9918a7abd9ba3107097a9fb2
Shah-king <sultanarekha378@gmail.com>
2025-01-12 21:35:28 -0500

    Update HomePage.vue

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -137,7 +137,7 @@
   },
   mounted() {
       // load school list for select drop down box
-      const SERVER_URL = 'http://44.212.9.12'
+      const SERVER_URL = 'http://44.212.9.12:80'
       fetch(process.env.SERVER_URL + '/api/school-list')
       .then(response => response.json())
       .then(schoolList => {
